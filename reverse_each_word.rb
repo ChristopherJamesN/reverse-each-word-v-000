ce4869a87750ff057d2ccb24bb3bf8017bef35de
Chris Nady <nady.christopher@gmail.com>
2017-09-06 15:50:44 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,7 @@
 def reverse_each_word(sentence)
   array = sentence.split(" ")
   new_array = []
-  array.each_with_index do |i,index|
-    new_array[index] = i.reverse
-  end
-  new_array.collect_with_index do |i,index|
-    if new_array.length != index + 1
-      "#{i} "
-    else
-      "#{i}"
-    end
+  array.collect do |i|
+    "i.reverse"
   end
 end
