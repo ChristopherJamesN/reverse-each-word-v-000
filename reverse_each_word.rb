872e43cc416ee12fe15726f532116f701b70c832
Chris Nady <nady.christopher@gmail.com>
2017-09-06 15:57:16 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   array = sentence.split(" ")
   new_sentence = String.new
   array.collect do |i|
-    new_sentence<<"#{i.reverse}"
+    new_sentence<<"#{i.reverse} "
   end
   new_sentence
 end
