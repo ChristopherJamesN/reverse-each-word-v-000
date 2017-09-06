9ffdbda85e65b30d4bb872a5e42df8d1182400a9
Chris Nady <nady.christopher@gmail.com>
2017-09-06 15:46:02 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(sentence)
   new_array = []
   array.each_with_index do |i,index|
     new_array[index] = i.reverse
-  end 
+  end
   new_sentence = String.new
   new_array.each_with_index do |i,index|
     new_sentence<<" #{i}"
